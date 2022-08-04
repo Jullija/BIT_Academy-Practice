b7cce8849e86e9953e7dc01677a5782118aa117b
Julija <juleczek2002@gmail.com>
2022-08-04 21:59:30 +0200

    robot commit

--- OOP/Task1/robot.py
+++ OOP/Task1/robot.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 class robotStatus:
     ALIVE = 0
     DEAD  = 1
@@ -8,262 +10,265 @@ BATTERY_VAL = 10
 class robot:
     # mapa, x, y, bateria
     def __init__(self, T, x, y, b):
-        self.__T = T
-        self.__x = x
-        self.__y = y
-        self.__b = b
-        self.__s = None #pole na status
+        self.__map      = T
+        self.__x        = x
+        self.__y        = y
+        self.__battery  = b
+        self.__status = robotStatus.ALIVE
+
+        if T[x][y] == "G":      
+            self.__status = robotStatus.CRASH
+        if T[x][y] == "W":     
+            self.__status = robotStatus.WATER
+        if T[x][y] == "B":
+            self.__set_battery(BATTERY_VAL)
+            self.__map[x][y] = "T"
+
+        if self.__battery <= 0: 
+            self.__status = robotStatus.DEAD
+        pass
+
+
+    def __isWall(self, T, x, y):
+        output = False
+        if x < 0:
+            output = True
+        if y < 0:           
+            output = True
+        if x >= len(T):     
+            output = True
+        if y >= len(T[0]):  
+            output = True
+        return output
+
 
     def left(self, val = 1):
-        robot_status = self.get_status()
-        robot_battery = self.get_battery()
+        x = self.get_x()
+        y = self.get_y()
+        b = self.get_battery()
+        m = self.__map
+        s = self.get_status()
 
-        if val > 0 and robot_status == robotStatus.ALIVE and robot_battery > 0:
-            map = self.get_map()
-            x = self.get_x()
-            y = self.get_y()
+        if s: 
+            return self
+        if val <= 0:
+            return self
+
+
+        destination = y - min(val, b)
+
+        
+        while y != destination:
+            y -= 1
+            self.__battery -= 1
             
-            iter = 0
-            while iter != val:
+            if self.__isWall(m, x, y):
+                self.__set_status(robotStatus.CRASH)
+                return self
 
-                while robot_battery > 0 and y - 1 >=0 :
-                    y -= 1
+            if m[x][y] == "G":
+                self.__y = y + 1 #bo chce wejść na góre, a nie wszedł na góre
+                self.__set_status(robotStatus.CRASH)
+                return self
 
-                    if map[x][y] == "T":
-                        robot_battery -= 1
-                    elif map[x][y] == "W":
-                        robot_battery -= 1
-                        self.set_battery(robot_battery)
-                        self.set_y(y)
-                        self.set_status(robotStatus.WATER)
-                        break
-                    elif map[x][y] == "G":
-                        robot_battery -= 1
-                        self.set_battery(robot_battery)
-                        self.set_y(y)
-                        self.set_status(robotStatus.CRASH)
-                        break
-                    else:
-                        robot_battery -= 1
-                        robot_battery += BATTERY_VAL
-                        map[x][y] = "T"
-                        self.__T[x][y] = "T"
-                    
-                    iter += 1
-            
+            if m[x][y] == "W":
+                self.__set_status(robotStatus.WATER)
+                self.__y = y 
+                return self
 
-                if iter != val: #nie udało się dotrzeć do końca
-                    if y - 1 >= 0: #mogłabym zrobić ruch, ale to oznacza, że nie miałam baterii
-                        self.set_battery(0)
-                        self.set_status(robotStatus.DEAD)
-                    else: #wyszłam poza mapę
-                        self.set_status(robotStatus.DEAD)
+            if m[x][y] == "B":
+                self.__set_map(x, y, "T")        
+                self.__set_battery(BATTERY_VAL)
+                
+            if self.__battery == 0:
+                self.__set_status(robotStatus.DEAD)
+                self._y = y
+                return self
 
-                        
+        self.__y = destination
+        return self
+
+
 
 
     def right(self, val = 1):
-        robot_status = self.get_status()
-        robot_battery = self.get_battery()
+        x = self.get_x()
+        y = self.get_y()
+        b = self.get_battery()
+        m = self.__map
+        s = self.get_status()
 
-        if val > 0 and robot_status == robotStatus.ALIVE and robot_battery > 0:
-            map = self.get_map()
-            x = self.get_x()
-            y = self.get_y()
+        if s: 
+            return self
+        if val <= 0:
+            return self
+
+
+        destination = y + min(val, b)
+        
+        while y != destination:
+            y += 1
+            self.__battery -= 1
             
-            iter = 0
-            while iter != val:
+            if self.__isWall(m, x, y):
+                self.__set_status(robotStatus.CRASH)
+                return self
 
-                while robot_battery > 0 and y + 1 < len(map[x]):
-                    y += 1
+            if m[x][y] == "G":
+                self.__y = y - 1
+                self.__set_status(robotStatus.CRASH)
+                return self
 
-                    if map[x][y] == "T":
-                        robot_battery -= 1
-                    elif map[x][y] == "W":
-                        robot_battery -= 1
-                        self.set_battery(robot_battery)
-                        self.set_y(y)
-                        self.set_status(robotStatus.WATER)
-                        break
-                    elif map[x][y] == "G":
-                        robot_battery -= 1
-                        self.set_battery(robot_battery)
-                        self.set_y(y)
-                        self.set_status(robotStatus.CRASH)
-                        break
-                    else:
-                        robot_battery -= 1
-                        robot_battery += BATTERY_VAL
-                        map[x][y] = "T"
-                        self.__T[x][y] = "T"
-                    
-                    iter += 1
-            
+            if m[x][y] == "W":
+                self.__set_status(robotStatus.WATER)
+                self.__y = y 
+                return self
 
-                if iter != val: #nie udało się dotrzeć do końca
-                    if y + 1 < len(map[x]): #mogłabym zrobić ruch, ale to oznacza, że nie miałam baterii
-                        self.set_battery(0)
-                        self.set_status(robotStatus.DEAD)
-                    else: #wyszłam poza mapę
-                        self.set_status(robotStatus.DEAD)
+            if m[x][y] == "B":
+                self.__set_map(x, y, "T")        
+                self.__set_battery(BATTERY_VAL)
+
+            if self.__battery == 0:
+                self.__set_status(robotStatus.DEAD)
+                self._y = y
+                return self
+
+        self.__y = destination
+        return self
+
+
 
 
     def up(self, val = 1):
-        robot_status = self.get_status()
-        robot_battery = self.get_battery()
+        x = self.get_x()
+        y = self.get_y()
+        b = self.get_battery()
+        m = self.__map
+        s = self.get_status()
 
-        if val > 0 and robot_status == robotStatus.ALIVE and robot_battery > 0:
-            map = self.get_map()
-            x = self.get_x()
-            y = self.get_y()
+        if s: 
+            return self
+        if val <= 0:
+            return self
+
+
+        destination = x - min(val, b)
+
+        
+        while x != destination:
+            x -= 1
+            self.__battery -= 1
             
-            iter = 0
-            while iter != val:
+            if self.__isWall(m, x, y):
+                self.__set_status(robotStatus.CRASH)
+                return self
 
-                while robot_battery > 0 and x - 1 >=0 :
-                    x -= 1
+            if m[x][y] == "G":
+                self.__x = x + 1
+                self.__set_status(robotStatus.CRASH)
+                return self
 
-                    if map[x][y] == "T":
-                        robot_battery -= 1
-                    elif map[x][y] == "W":
-                        robot_battery -= 1
-                        self.set_battery(robot_battery)
-                        self.set_y(y)
-                        self.set_status(robotStatus.WATER)
-                        break
-                    elif map[x][y] == "G":
-                        robot_battery -= 1
-                        self.set_battery(robot_battery)
-                        self.set_y(y)
-                        self.set_status(robotStatus.CRASH)
-                        break
-                    else:
-                        robot_battery -= 1
-                        robot_battery += BATTERY_VAL
-                        map[x][y] = "T"
-                        self.__T[x][y] = "T"
-                    
-                    iter += 1
-            
+            if m[x][y] == "W":
+                self.__set_status(robotStatus.WATER)
+                self.__x = x 
+                return self
 
-                if iter != val: #nie udało się dotrzeć do końca
-                    if x - 1 >= 0: #mogłabym zrobić ruch, ale to oznacza, że nie miałam baterii
-                        self.set_battery(0)
-                        self.set_status(robotStatus.DEAD)
-                    else: #wyszłam poza mapę
-                        self.set_status(robotStatus.DEAD)
+            if m[x][y] == "B":
+                self.__set_map(x, y, "T")        
+                self.__set_battery(BATTERY_VAL)
+
+            if self.__battery == 0:
+                self.__set_status(robotStatus.DEAD)
+                self._x = x
+                return self
+
+        self.__x = destination
+        return self
 
 
 
     def down(self, val = 1):
-        robot_status = self.get_status()
-        robot_battery = self.get_battery()
-
-        if val > 0 and robot_status == robotStatus.ALIVE and robot_battery > 0:
-            map = self.get_map()
-            x = self.get_x()
-            y = self.get_y()
-            
-            iter = 0
-            while iter != val:
-
-                while robot_battery > 0 and x + 1 <len(map[y]) :
-                    x += 1
-
-                    if map[x][y] == "T":
-                        robot_battery -= 1
-                    elif map[x][y] == "W":
-                        robot_battery -= 1
-                        self.set_battery(robot_battery)
-                        self.set_y(y)
-                        self.set_status(robotStatus.WATER)
-                        break
-                    elif map[x][y] == "G":
-                        robot_battery -= 1
-                        self.set_battery(robot_battery)
-                        self.set_y(y)
-                        self.set_status(robotStatus.CRASH)
-                        break
-                    else:
-                        robot_battery -= 1
-                        robot_battery += BATTERY_VAL
-                        map[x][y] = "T"
-                        self.__T[x][y] = "T"
-                    
-                    iter += 1
-            
-
-                if iter != val: #nie udało się dotrzeć do końca
-                    if x + 1 < len(map[y]): #mogłabym zrobić ruch, ale to oznacza, że nie miałam baterii
-                        self.set_battery(0)
-                        self.set_status(robotStatus.DEAD)
-                    else: #wyszłam poza mapę
-                        self.set_status(robotStatus.DEAD)
-
-
-
-
-
-
-
-    def set_status(self, stat):
-        self.__s = stat
-    
-    def set_battery(self, val):
-        self.__b = val
-
-    def set_x(self, val):
-        self.__x = val
-
-    def set_y(self, val):
-        self.__y = val
-
-
-
-
-
-
-
-
-
-    def get_status(self):
-        if self.__s == None:
-            if self.get_battery() == 0:
-                return robotStatus.DEAD
-
-            T = self.__T
-            x = self.get_x()
-            y = self.get_y()
-
-            if T[x][y] == "T":
-                return robotStatus.ALIVE
-            elif T[x][y] == "W":
-                return robotStatus.WATER
-            else:
-                return robotStatus.CRASH
-        return self.__s
-
-
-
-    def get_battery(self):
-        return self.__b
-
-    def get_map(self):
-        robot_status = self.get_status()
         x = self.get_x()
         y = self.get_y()
-        T = self.__T
+        b = self.get_battery()
+        m = self.__map
+        s = self.get_status()
 
-        if robot_status == robotStatus.ALIVE:
-            T[x][y] = "R"
+        if s: 
+            return self
+        if val <= 0:
+            return self
+
+
+        destination = x + min(val, b)
+        
+        while x != destination:
+            x += 1
+            self.__battery -= 1
+            
+            if self.__isWall(m, x, y):
+                self.__set_status(robotStatus.CRASH)
+                return self
+
+            if m[x][y] == "G":
+                self.__x = x - 1
+                self.__set_status(robotStatus.CRASH)
+                return self
+
+            if m[x][y] == "W":
+                self.__set_status(robotStatus.WATER)
+                self.__x = x 
+                return self
+
+            if m[x][y] == "B":
+                self.__set_map(x, y, "T")        
+                self.__set_battery(BATTERY_VAL)
+                
+            if self.__battery == 0:
+                self.__set_status(robotStatus.DEAD)
+                self._x = x
+                return self
+
+        self.__x = destination
+        return self
+
+        
+
+
+
+    def __set_status(self, stat):
+        self.__status = stat
+        return
+    def __set_battery(self, val):
+        self.__battery += val
+        return
+    def __set_x(self, val):
+        self.__x += val
+        return
+    def __set_y(self, val):
+        self.__y += val
+        return
+    def __set_map(self, x, y, val):
+        self.__map[x][y] = val
+        return
+
+    def get_status(self):
+        return self.__status
+    def get_battery(self):
+        return self.__battery
+    def __get_u_map(self):
+        return self.__map
+    def get_map(self):
+        output = deepcopy(self.__get_u_map())
+        if self.get_status() == robotStatus.ALIVE:
+            output[self.get_x()][self.get_y()] = "R"
         else:
-            T[x][y] = "X"
-        return T
-
-
-
+            output[self.get_x()][self.get_y()] = "X"
+        return output
     def get_x(self):
         return self.__x
-        
     def get_y(self):
         return self.__y
+
+
+        
